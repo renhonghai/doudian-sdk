607e13320fe6edb363b59dbda97960305427c13b
renhonghai <renhonghai@me.com>
2021-12-10 16:00:03 +0800

    新增售后常量

---
+++ src/Constants/AfterSaleStatusConstant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AK\DoudianSDK\Constants;
+
+class AfterSaleStatusConstant
+{
+
+    /**
+     * 枚举为6(待商家同意),7(待买家退货),11(待商家二次同意),12(售后成功),13(换货待买家收货),14(换货成功),27(商家一次拒绝),28(售后失败),29(商家二次拒绝)
+     */
+    const TYPE_PENDING = 6;
+    const TYPE_SUCCESSFUL = 12;
+
+}
