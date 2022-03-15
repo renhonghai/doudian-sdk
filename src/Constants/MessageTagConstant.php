8fef7d00027b244eef40a97f4cefb58b9b96c1ed
renhonghai <renhonghai@me.com>
2022-03-15 17:14:37 +0800

    添加MessageTag: 208

--- src/Constants/MessageTagConstant.php
+++ src/Constants/MessageTagConstant.php
@@ -12,11 +12,13 @@ class MessageTagConstant
      * 103: 确认收货
      * 200: 买家申请售后(仅退款)
      * 201: 同意退款
+     * 208: 买家修改售后申请消息
      */
     const TRADE_CREATE = "100";
     const TRADE_PAID = "101";
     const TRADE_SUCCESS = "103";
     const REFUND_CREATED = "200";
     const REFUND_AGREED = "201";
+    const REFUND_MODIFIED = "208";
 
 }
